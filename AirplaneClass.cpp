91afe54501162c08932c32d09ef1857e2b7b4afc
Samuel <34661120+The-Chemical-Engineer@users.noreply.github.com>
2017-12-18 12:28:00 -0800

    Add files via upload

---
+++ AirplaneClass.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+class Airplane
+{
+    public:
+    
+    private:
+    
+    //Constructor
+    Airplane();
+    
+    //ID of every airplane
+    void airplaneID(){
+        string plane1 = "Jetblue";
+        string plane2 = "American Airlines";
+        string plane3 = "Alaskan Airlines";
+        string plane4 = "Ghetto Flights";
+        string plane5 = "United Airlines";
+    }
+    
+    //Function which loads up every passenger
+    int passengerLoad(int passengers);
+    
+    //Shows which route the plane will take
+    int routeTaken();
+    
+    //Shows the location of every plane
+    string location();
+    
+    
+    //Destructor used to delete any dynamically allocated memory
+    ~Airplane();
+    
+};
