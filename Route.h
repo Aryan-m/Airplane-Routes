b809fdc1567be575f47f489878f5f481c8b629b5
kmaroufkhani <34661650+kmaroufkhani@users.noreply.github.com>
2017-12-21 12:40:13 -0800

    Update Route.h

--- Route.h
+++ Route.h
@@ -4,15 +4,9 @@
 class Route
 {
 	public:
-		int id; //route id number
-		int cost; //cost of route
-		int duration; //duration of route
-		int dep; //departure location
-		int dest; //destination location
-		int wt_dep; //weather at departure
-		int wt_dest; //weather at destination
-		int time; //time
-		int plane; //what plane will go through this route
+	int rdID; //ID of roads
+	int dist; //distance of each road
+	int time; //duration of each road
 	protected:
 };
 
